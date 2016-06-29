f687da18ea21730c024eaaa0c43e6ad8d6b7ecb8
Robert W Boyer <rwboyer@mac.com>
2016-06-29 16:08:34 -0400

    initial

---
+++ js/manifest.js
@@ -0,0 +1,8 @@
+// !All JS
+//= include ../bower_components/jquery/dist/jquery.js
+//= include ../js/src/nav.js 
+//= include ../js/src/expander.js
+//= include ../js/src/modal.js
+//= include ../js/src/fadein.js
+//= include ../js/src/parallax.js
+//= include ../js/src/dynamic-height.js
---
+++ js/src/dynamic-height.js
@@ -0,0 +1,5 @@
+
+$(window).on('resize', function(){
+  var cw = $('.big-block').width();
+  $('.big-block').css({'height':cw+'px'});
+});

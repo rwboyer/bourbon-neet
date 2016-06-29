3d31ca51a5cf6ff93dc6514e691b9f8824bed078
Robert W Boyer <rwboyer@mac.com>
2016-06-29 16:15:01 -0400

    fire resize

--- js/src/dynamic-height.js
+++ js/src/dynamic-height.js
@@ -3,3 +3,7 @@ $(window).on('resize', function(){
   var cw = $('.big-block').width();
   $('.big-block').css({'height':cw+'px'});
 });
+
+$(document).ready(function(){
+  $(window).trigger('resize');
+})
